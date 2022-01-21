a024ab7eed7dcf0d5a9aaba6e26c6a0058e9811d
Suchitra <suchitrasrinivas10@gmail.com>
2022-01-21 08:18:32 +0530

    Formatted the code

--- LinkedList-Practice/src/com/sample/test/practice/PalindromeLinkedList.java
+++ LinkedList-Practice/src/com/sample/test/practice/PalindromeLinkedList.java
@@ -15,67 +15,65 @@ import java.util.Stack;
  * 
  */
 
-
 class ListNode {
-	     int val;
-	     ListNode next;
-	    ListNode() {}
-	    ListNode(int val) { this.val = val; }
-	    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	int val;
+	ListNode next;
+
+	ListNode() {
+	}
+
+	ListNode(int val) {
+		this.val = val;
+	}
+
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
 }
 
 public class PalindromeLinkedList {
 
-	
 	public boolean isPalindrome(ListNode head) {
 		ListNode temp = head;
 		Stack<Integer> visited = new Stack<>();
 		boolean isPalindrome = true;
-		
-		// Pushing all the linked list nodes into the stack until we reach the end of the Linked list
-		while(temp !=null) {
+
+		// Pushing all the linked list nodes into the stack until we reach the end of
+		// the Linked list
+		while (temp != null) {
 			visited.push(temp.val);
 			temp = temp.next;
 		}
-		while(head !=null) {
+		while (head != null) {
 			int i = visited.pop();
-			if ( i == head.val) {
+			if (i == head.val) {
 				isPalindrome = true;
-			}else {
+			} else {
 				isPalindrome = false;
 				break;
 			}
-			head = head.next;	
-			
-			
+			head = head.next;
+
 		}
-		
-		
-		
+
 		return isPalindrome;
-		
-		
-		
+
 	}
-		    
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		ListNode head = new ListNode(1);
 		head.next = new ListNode(2);
 		head.next.next = new ListNode(2);
 		head.next.next.next = new ListNode(1);
 		PalindromeLinkedList pll = new PalindromeLinkedList();
 		System.out.println("Is it a palindrome ? :" + pll.isPalindrome(head));
-		
-		
+
 		ListNode head1 = new ListNode(1);
 		head1.next = new ListNode(2);
-		System.out.println("Is it a palindrome ? : "+ pll.isPalindrome(head1));
-		
-		
-		
+		System.out.println("Is it a palindrome ? : " + pll.isPalindrome(head1));
 
 	}
 

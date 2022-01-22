a80a9e0ea4c8594f50b7c74981b8ff4327e520bd
Suchitra <suchitrasrinivas10@gmail.com>
2022-01-22 06:41:50 +0530

    Added code for rearrangement of linked list nodes based on odd-even

---
+++ LinkedList-Practice/src/com/sample/test/practice/OddEvenLinkedList.java
@@ -0,0 +1,62 @@
+package com.sample.test.practice;
+
+/*
+ * Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+ 
+ Input :- 1-->2-->3-->4-->5--> null
+ 
+ Output:- 1-->3-->5-->2-->4--> null
+ */
+
+public class OddEvenLinkedList {
+
+	public ListNode rearrangeOddEven(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		ListNode oddHead = head;
+		ListNode evenStart = head.next;
+		ListNode evenHead = head.next;
+
+		while (evenHead != null) {
+			oddHead.next = evenHead.next;
+			if (oddHead.next != null) {
+				oddHead = oddHead.next;
+			}
+
+			evenHead.next = oddHead.next;
+			evenHead = evenHead.next;
+
+		}
+		oddHead.next = evenStart;
+		return head;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+
+		OddEvenLinkedList oddEven = new OddEvenLinkedList();
+		oddEven.rearrangeOddEven(head);
+		ListNode temp = head;
+		while (temp != null) {
+			System.out.print(temp.val + "|");
+			temp = temp.next;
+		}
+
+	}
+
+}
